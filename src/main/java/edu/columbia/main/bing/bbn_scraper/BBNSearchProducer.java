f9f8d69c6386db06b6e3978f39a1be48b9ca8147
Victor Soto <vsoto@cs.columbia.edu>
2018-04-03 10:34:42 -0400

    Debug

--- src/main/java/edu/columbia/main/bing/bbn_scraper/BBNSearchProducer.java
+++ src/main/java/edu/columbia/main/bing/bbn_scraper/BBNSearchProducer.java
@@ -34,8 +34,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * Created by Gideon on 4/24/15.
  */
-
-
 /**
  * Searches bing API for a word and puts all results into broker
  */
@@ -51,7 +49,6 @@ public class BBNSearchProducer extends BabelProducer {
     static String host = "https://api.cognitive.microsoft.com";
     static String path = "/bing/v7.0/search";
 
-
     static AtomicInteger numOfRequests;
     Logger log = Logger.getLogger(BBNSearchProducer.class);
     int ngram = BabelConfig.getInstance().getConfigFromFile().ngram();
@@ -59,14 +56,14 @@ public class BBNSearchProducer extends BabelProducer {
     public BBNSearchProducer(BabelBroker broker, String language, String ranked_ngrams_filename) {
         this.broker = broker;
         this.lang = language;
-        this.words = InitialDocumentCountRetriever.getHighestRankedNGrams(ranked_ngrams_filename, 50);
-        this.logDb =  new LogDB(this.lang);
+        this.words = InitialDocumentCountRetriever.getHighestRankedNGrams(ranked_ngrams_filename, 2);
+        this.logDb = new LogDB(this.lang);
         numOfRequests = new AtomicInteger();
     }
 
-    private static SearchResults SearchWeb (String searchQuery, String count, String offset) throws Exception {
-		URL url = new URL(host + path + "?q=" +  URLEncoder.encode(searchQuery, "UTF-8") + "&count=" + count +"&offset=" + offset);
-        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+    private static SearchResults SearchWeb(String searchQuery, String count, String offset) throws Exception {
+        URL url = new URL(host + path + "?q=" + URLEncoder.encode(searchQuery, "UTF-8") + "&count=" + count + "&offset=" + offset);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
         connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
 
         // receive JSON body
@@ -79,12 +76,14 @@ public class BBNSearchProducer extends BabelProducer {
         // extract Bing-related HTTP headers
         Map<String, List<String>> headers = connection.getHeaderFields();
         for (String header : headers.keySet()) {
-			if (header == null) continue;      // may have null key
-			if (header.startsWith("BingAPIs-") || header.startsWith("X-MSEdge-")) {
-				results.relevantHeaders.put(header, headers.get(header).get(0));
-			}
-		}
-		stream.close();
+            if (header == null) {
+                continue;      // may have null key
+            }
+            if (header.startsWith("BingAPIs-") || header.startsWith("X-MSEdge-")) {
+                results.relevantHeaders.put(header, headers.get(header).get(0));
+            }
+        }
+        stream.close();
         return results;
     }
 
@@ -96,59 +95,61 @@ public class BBNSearchProducer extends BabelProducer {
         return gson.toJson(json);
     }
 
-	
-	// pretty-printer for JSON; uses GSON parser to parse and re-serialize
+    // pretty-printer for JSON; uses GSON parser to parse and re-serialize
     private static ArrayList<String> getURLs(String json_text) {
-		JsonParser parser = new JsonParser();
-		JsonObject json = parser.parse(json_text).getAsJsonObject();
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		ArrayList<String> urls = new ArrayList<String>();
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(json_text).getAsJsonObject();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        ArrayList<String> urls = new ArrayList<String>();
 
-		if(json.has("webPages")) {
-			JsonObject webPages = json.getAsJsonObject("webPages"); 
-			for (JsonElement entry : webPages.getAsJsonArray("value")) {
-				JsonObject result = entry.getAsJsonObject();
-				String url = result.getAsJsonPrimitive("url").getAsString();
-				System.out.println(url);
-				urls.add(url);
-			}
-		}
-		return urls;
+        if (json.has("webPages")) {
+            JsonObject webPages = json.getAsJsonObject("webPages");
+            for (JsonElement entry : webPages.getAsJsonArray("value")) {
+                JsonObject result = entry.getAsJsonObject();
+                String url = result.getAsJsonPrimitive("url").getAsString();
+                System.out.println(url);
+                urls.add(url);
+            }
+        }
+        return urls;
     }
 
     @Override
-    protected void searchWordAndSave(String ngram){
-		boolean breakFlag = false;
-		int counter = 0;
-		this.subscriptionKey = BabelConfig.getInstance().getConfigFromFile().bing();
-		// String searchQuery = "site:blogspot.com " + " \""+word+"\"" + " NOT lang:en";
-		String searchQuery = "\""+ngram+"\"" + " NOT lang:en";
-		try {
-			for (int i=0; !breakFlag ; i++) {
-				SearchResults result = this.SearchWeb(searchQuery, "50", String.valueOf(i*50));
-				ArrayList<String> urls = getURLs(result.jsonResponse);
-				if (counter++ == 100 || urls.size() == 0)
-					breakFlag = true;
-				numOfRequests.getAndIncrement();
+    protected void searchWordAndSave(String ngram) {
+        boolean breakFlag = false;
+        int counter = 0;
+        this.subscriptionKey = BabelConfig.getInstance().getConfigFromFile().bing();
+        // String searchQuery = "site:blogspot.com " + " \""+word+"\"" + " NOT lang:en";
+        String searchQuery = "\"" + ngram + "\"" + " NOT lang:en";
+        try {
+            System.out.println("Querying: " + ngram);
+            for (int i = 0; !breakFlag; i++) {
+                System.out.println("Page: " + i);
+                SearchResults result = this.SearchWeb(searchQuery, "50", String.valueOf(i * 50));
+                ArrayList<String> urls = getURLs(result.jsonResponse);
+                if (counter++ == 100 || urls.size() == 0) {
+                    breakFlag = true;
+                }
+                numOfRequests.getAndIncrement();
 
-				for (String url: urls) {
-					BBNJob job = new BBNJob(url, lang, logDb);
-					try {
-						if (job.isValid())
-							broker.put(job);
-						else
-							log.debug("Job not valid: " + job);
-					} catch (InterruptedException e) {
-						log.error(e);
-					}
-				}
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace(System.out);
-			System.exit(1);
-		}
-	// The results are paged. You can get 50 results per page max.
+                for (String url : urls) {
+                    BBNJob job = new BBNJob(url, lang, logDb);
+                    try {
+                        if (job.isValid()) {
+                            broker.put(job);
+                        } else {
+                            log.debug("Job not valid: " + job);
+                        }
+                    } catch (InterruptedException e) {
+                        log.error(e);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            System.exit(1);
+        }
+        // The results are paged. You can get 50 results per page max.
         //for (int i=1; !breakFlag ; i++) {
         //    aq.setPage(i);
         //    aq.doQuery();
@@ -173,9 +174,11 @@ public class BBNSearchProducer extends BabelProducer {
 }
 
 // Container class for search results encapsulates relevant headers and JSON data
-class SearchResults{
+class SearchResults {
+
     HashMap<String, String> relevantHeaders;
     String jsonResponse;
+
     SearchResults(HashMap<String, String> headers, String json) {
         relevantHeaders = headers;
         jsonResponse = json;
