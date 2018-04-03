0be58f8690164b4b850a27230adb1443f8ac509c
Victor Soto <vsoto@cs.columbia.edu>
2018-04-03 13:31:59 -0400

    Debug

--- src/main/java/edu/columbia/main/collection/SoupScraper.java
+++ src/main/java/edu/columbia/main/collection/SoupScraper.java
@@ -22,10 +22,9 @@ import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
- * This class scrapes data from a RSS feed, checks that it is in the desired languageCode and finally saves it
- * Created by Gideon on 9/28/14.
+ * This class scrapes data from a RSS feed, checks that it is in the desired
+ * languageCode and finally saves it Created by Gideon on 9/28/14.
  *
  */
 public class SoupScraper {
@@ -46,104 +45,75 @@ public class SoupScraper {
 
     }
 
-
     public AbstractMap.SimpleEntry<Integer, Integer> fetchAndSave() throws Exception {
 
         URL url = new URL(this.url);
         System.out.println("Fetching and saving " + this.url);
-        
-        SyndFeedInput input = new SyndFeedInput();
-        SyndFeed feed = input.build(new XmlReader(url));
 
+        Document doc = Jsoup.connect(this.url).get();
+        String title = doc.title();
+        String content = doc.text();
 
-        int items = feed.getEntries().size();
-        System.out.println("About to scrape " + this.url);
-        
-        if(items > 0){
-            log.info("Attempting to parse rss feed: "+ this.url );
-            log.info("This Feed has "+items +" items");
-        }
-
-        List <SyndEntry> entries = feed.getEntries();
-
-        for (SyndEntry item : entries){
-            log.info("Title: " + item.getTitle());
-            log.info("Link: " + item.getLink());
-            SyndContentImpl contentHolder = (SyndContentImpl) item.getContents().get(0);
-            String content = contentHolder.getValue();
-
-            //content might contain html data, let's clean it up
-            Document doc = Jsoup.parse(content);
-            content = doc.text();
-            try {
-                    Result result = ld.detectLanguage(content, language);
-                    if (result.languageCode.equals(language) && result.isReliable) {
-
-                        FileSaver file = new FileSaver(content, this.language, "soup", item.getLink(), item.getUri(), String.valueOf(content.hashCode()));
-                        String fileName = file.getFileName();
-                        Post post = new Post(content, this.language, null, "soup", item.getLink(), item.getUri(), fileName);
-                        if(DAO.saveEntry(post)) {
-                            file.save(this.logDb);
-                            numOfFiles++;
-                            wrongCount = 0;
-                        }
-
-                    }
-
-                    else{
-                        log.info("Item " + item.getTitle() + "is in a diff languageCode, skipping this post  "+ result.languageCode);
-                        wrongCount ++;
-                        if(wrongCount > 3){
-                            log.info("Already found 3 posts in the wrong languageCode, skipping this blog");
-                        }
-                        break;
-                    }
-
-            }
-            catch(Exception e){
-                log.error(e);
-                break;
-            }
-
-
-        }
-        return new AbstractMap.SimpleEntry<>(numOfFiles,wrongCount);
-    }
-
-    public static List getAllPostsFromFeed(String urlToGet, String source) throws IOException, FeedException {
-
-        ArrayList<BlogPost> posts = new ArrayList<BlogPost>();
-
-        URL url = new URL(urlToGet);
-        SyndFeedInput input = new SyndFeedInput();
         try {
-            SyndFeed feed = input.build(new XmlReader(url));
-
-            int items = feed.getEntries().size();
-
-            if (items > 0) {
-                log.info("Attempting to parse rss feed: " + urlToGet);
-                log.info("This Feed has " + items + " items");
-                List<SyndEntry> entries = feed.getEntries();
-
-                for (SyndEntry item : entries) {
-                    if (item.getContents().size() > 0) {
-                        SyndContentImpl contentHolder = (SyndContentImpl) item.getContents().get(0);
-                        String content = contentHolder.getValue();
-                        if (content != null && !content.isEmpty()) {
-                            BlogPost post = new BlogPost(content, null, null, source, item.getLink(), item.getUri(), null);
-                            posts.add(post);
-                        }
-                    }
+            Result result = ld.detectLanguage(content, language);
+            if (result.languageCode.equals(language) && result.isReliable) {
+                FileSaver file = new FileSaver(content, this.language, "soup", this.url, this.url, String.valueOf(content.hashCode()));
+                String fileName = file.getFileName();
+                BlogPost post = new BlogPost(content, this.language, null, "soup", this.url, this.url, fileName);
+                if (DAO.saveEntry(post)) {
+                    file.save(this.logDb);
+                    numOfFiles++;
+                    wrongCount = 0;
                 }
-            }
-            return posts;
-        }
-        catch(Exception ex){
-            log.error(ex);
-            return posts;
-        }
 
+            } else {
+                log.info("Item " + title + "is in a diff languageCode, skipping this post  " + result.languageCode);
+                wrongCount++;
+                if (wrongCount > 3) {
+                    log.info("Already found 3 posts in the wrong languageCode, skipping this blog");
+                }
+                
+            }
+
+        } catch (Exception e) {
+            log.error(e);
+        }
+        return new AbstractMap.SimpleEntry<>(numOfFiles, wrongCount);
     }
+
+//    public static List getAllPostsFromFeed(String urlToGet, String source) throws IOException, FeedException {
+//
+//        ArrayList<BlogPost> posts = new ArrayList<BlogPost>();
+//
+//        URL url = new URL(urlToGet);
+//        SyndFeedInput input = new SyndFeedInput();
+//        try {
+//            SyndFeed feed = input.build(new XmlReader(url));
+//
+//            int items = feed.getEntries().size();
+//
+//            if (items > 0) {
+//                log.info("Attempting to parse rss feed: " + urlToGet);
+//                log.info("This Feed has " + items + " items");
+//                List<SyndEntry> entries = feed.getEntries();
+//
+//                for (SyndEntry item : entries) {
+//                    if (item.getContents().size() > 0) {
+//                        SyndContentImpl contentHolder = (SyndContentImpl) item.getContents().get(0);
+//                        String content = contentHolder.getValue();
+//                        if (content != null && !content.isEmpty()) {
+//                            BlogPost post = new BlogPost(content, null, null, source, item.getLink(), item.getUri(), null);
+//                            posts.add(post);
+//                        }
+//                    }
+//                }
+//            }
+//            return posts;
+//        } catch (Exception ex) {
+//            log.error(ex);
+//            return posts;
+//        }
+//
+//    }
 
 }
