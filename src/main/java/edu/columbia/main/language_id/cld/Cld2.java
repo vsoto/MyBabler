8be5527a6430002013c7d14d9945db6df5dbae03
Victor Soto <vsoto@cs.columbia.edu>
2018-09-09 10:21:56 +0530

    Debug

--- src/main/java/edu/columbia/main/language_id/cld/Cld2.java
+++ src/main/java/edu/columbia/main/language_id/cld/Cld2.java
@@ -25,70 +25,66 @@ import java.io.IOException;
  */
 public class Cld2 extends LanguageClassifier {
 
-
     public Cld2() throws IOException, ClassNotFoundException {
         this.detectLanguage("test that will throw an error if fails to load cld");
-        String [] listOfLangs = {"Afrikaans","Albanian","Arabic","Armenian","Azerbaijani","Basque","Belarusian","Bengali","Bihari languages","Bulgarian","Catalan","Cebuano","Cherokee","Croatian","Czech","Chinese","Danish","Dhivehi","Dutch","English","Estonian","Finnish","French","Galician","Ganda","Georgian","German","Modern Greek","Gujarati","Haitian","Hebrew","Hindi","Hmong","Hungarian","Icelandic","Indonesian","Inuktitut","Irish","Italian","Javanese","Japanese","Kannada","Central Khmer","Kinyarwanda","Korean","Lao","Latvian","Lithuanian","Macedonian","Malay","Malayalam","Maltese","Marathi","Nepali","Norwegian","Oriya","Persian","Polish","Portuguese","Panjabi","Romanian","Russian","Scottish Gaelic","Serbian","Sinhala","Slovak","Slovene","Spanish","Swahili","Swedish","Syriac","Tagalog","Tamil","Telugu","Thai","Turkish","Ukrainian","Urdu","Vietnamese","Welsh","Yiddish"};
+        String[] listOfLangs = {"Afrikaans", "Albanian", "Arabic", "Armenian", "Azerbaijani", "Basque", "Belarusian", "Bengali", "Bihari", "Bulgarian", "Catalan", "Cebuano", "Cherokee", "Croatian", "Czech", "Chinese", "Danish", "Dhivehi", "Dutch", "English", "Estonian", "Finnish", "French", "Galician", "Ganda", "Georgian", "German", "Modern Greek", "Gujarati", "Haitian", "Hebrew", "Hindi", "Hmong", "Hungarian", "Icelandic", "Indonesian", "Inuktitut", "Irish", "Italian", "Javanese", "Japanese", "Kannada", "Central Khmer", "Kinyarwanda", "Korean", "Lao", "Latvian", "Lithuanian", "Macedonian", "Malay", "Malayalam", "Maltese", "Marathi", "Nepali", "Norwegian", "Oriya", "Persian", "Polish", "Portuguese", "Panjabi", "Romanian", "Russian", "Scottish Gaelic", "Serbian", "Sinhala", "Slovak", "Slovene", "Somali", "Spanish", "Swahili", "Swedish", "Syriac", "Tagalog", "Tamil", "Telugu", "Thai", "Turkish", "Ukrainian", "Urdu", "Vietnamese", "Welsh", "Yiddish"};
         buildListOfSupportedLanguageCodesFromLanguageNames(listOfLangs);
 
     }
 
     public static int getLanguageFromName(String name) {
-    return Cld2Library.INSTANCE._ZN4CLD219GetLanguageFromNameEPKc(name);
+        return Cld2Library.INSTANCE._ZN4CLD219GetLanguageFromNameEPKc(name);
     }
 
     public static String getLanguageName(int language) {
-    return Cld2Library.INSTANCE._ZN4CLD212LanguageNameENS_8LanguageE(language);
+        return Cld2Library.INSTANCE._ZN4CLD212LanguageNameENS_8LanguageE(language);
     }
 
     public static String getLanguageCode(int language) {
-    return Cld2Library.INSTANCE._ZN4CLD212LanguageCodeENS_8LanguageE(language);
+        return Cld2Library.INSTANCE._ZN4CLD212LanguageCodeENS_8LanguageE(language);
     }
 
     public static String version() {
-    return Cld2Library.INSTANCE._ZN4CLD221DetectLanguageVersionEv();
+        return Cld2Library.INSTANCE._ZN4CLD221DetectLanguageVersionEv();
     }
 
+    public static edu.columbia.main.language_id.Result detect(String text) throws IOException, ClassNotFoundException {
+        boolean isPlainText = true;
 
+        CLDHints cldHints = new CLDHints(
+                null,
+                "",
+                Encoding.UNKNOWN_ENCODING,
+                Language.UNKNOWN_LANGUAGE);
 
-    public static edu.columbia.main.language_id.Result detect(String text) throws IOException, ClassNotFoundException{
-      boolean isPlainText = true;
+        int flags = 0;
+        int[] language3 = new int[3];
+        int[] percent3 = new int[3];
+        double[] normalizedScore3 = new double[3];
+        int[] textBytes = new int[1];
+        boolean[] isReliable = new boolean[1];
+        byte[] utf8EncodedText;
+        try {
+            utf8EncodedText = text.getBytes("UTF-8");
+        } catch (java.io.UnsupportedEncodingException exc) {
+            return new edu.columbia.main.language_id.Result(null, false, 0, "cld2");
 
-      CLDHints cldHints = new CLDHints(
-              null,
-              "",
-              Encoding.UNKNOWN_ENCODING,
-              Language.UNKNOWN_LANGUAGE);
+        }
+        int language = Cld2Library.INSTANCE._ZN4CLD224ExtDetectLanguageSummaryEPKcibPKNS_8CLDHintsEiPNS_8LanguageEPiPdPSt6vectorINS_11ResultChunkESaISA_EES7_Pb(
+                utf8EncodedText,
+                utf8EncodedText.length,
+                isPlainText,
+                cldHints,
+                flags,
+                language3,
+                percent3,
+                normalizedScore3,
+                null, // Supposed to be a vector of ResultChunks, but it is not direct to pass vectors.
+                textBytes,
+                isReliable);
 
-
-      int flags = 0;
-      int[] language3 = new int[3];
-      int[] percent3 = new int[3];
-      double[] normalizedScore3 = new double[3];
-      int[] textBytes = new int[1];
-      boolean[] isReliable = new boolean[1];
-      byte[] utf8EncodedText;
-      try {
-          utf8EncodedText = text.getBytes("UTF-8");
-      } catch (java.io.UnsupportedEncodingException exc) {
-          return new edu.columbia.main.language_id.Result(null, false, 0, "cld2");
-
-      }
-      int language = Cld2Library.INSTANCE._ZN4CLD224ExtDetectLanguageSummaryEPKcibPKNS_8CLDHintsEiPNS_8LanguageEPiPdPSt6vectorINS_11ResultChunkESaISA_EES7_Pb(
-              utf8EncodedText,
-              utf8EncodedText.length,
-              isPlainText,
-              cldHints,
-              flags,
-              language3,
-              percent3,
-              normalizedScore3,
-              null, // Supposed to be a vector of ResultChunks, but it is not direct to pass vectors.
-              textBytes,
-              isReliable);
-
-      LanguageCode lc = new LanguageCode(getLanguageCode(language), LanguageCode.CodeTypes.ISO_639_1);
-      return new edu.columbia.main.language_id.Result(lc.getLanguageCode(), isReliable[0], percent3[0] / 100.0, "cld2");
+        LanguageCode lc = new LanguageCode(getLanguageCode(language), LanguageCode.CodeTypes.ISO_639_1);
+        return new edu.columbia.main.language_id.Result(lc.getLanguageCode(), isReliable[0], percent3[0] / 100.0, "cld2");
 
     }
 
